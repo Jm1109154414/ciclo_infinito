72eb4b1e388ade111bfc4842fae9dc7740e85d4b
JoseMiguelrdz <A01646679@tec.mx>
2025-12-02 07:36:57 -0600

    ciclo infinito Hola mundo

---
+++ ciclo_infinito.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void) {
+    while (1) {                 // ciclo infinito
+        printf("Hola mundo\n"); // imprime el mensaje
+    }
+
+    return 0; // nunca se llega aquí
+}
